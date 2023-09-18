a9fb02322f93d57eb50833cc809dfb0594a92c29
Arekemaseayomide <92256623+Arekemaseayomide@users.noreply.github.com>
2023-09-18 20:00:00 +0100

    Update Project.jsx

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -14,10 +14,10 @@ const Project = () => {
                         src="/assets/images/a1ge.png"
                         alt="yj"
                         />
-                        <h4 className="text">
+                        <p className="text">
                         A Single Page Expense Dashboard that shows the transactions,
                         budgeting, savings and other services on a user's account.
-                        </h4>
+                        </p>
                         <NavLink className="btns" to="https://areke-ayo-expense-dashboard.netlify.app/">
                         View Project Live
                         </NavLink>
@@ -30,9 +30,9 @@ const Project = () => {
                         src="/assets/images/estate.png"
                         alt="yj"
                         />
-                        <h4 className="text">
+                        <p className="text">
                        A Real Estate web application that caters to buyers in different countries, each with distinct price ranges. Built with React.js and Vanilla CSS. 
-                        </h4>
+                        </p>
                         <NavLink className="btns" to="https://areke-ayo-real-estate.netlify.app/">
                         View Project Live
                         </NavLink>
@@ -45,9 +45,9 @@ const Project = () => {
                         src="/assets/images/weather.png"
                         alt="yj"
                         />
-                        <h4 className="text">
+                        <p className="text">
                         A User Friendly Weather web app that allows users to check the current weather conditions in different cities around the world. Built using React.js and OpenWeather API. 
-                        </h4>
+                        </p>
                         <NavLink className="btns" to="https://areke-ayo-weather-forecast-app.netlify.app/">
                         View Project Live
                         </NavLink>
@@ -60,9 +60,9 @@ const Project = () => {
                         src="/assets/images/commerce.png"
                         alt="yj"
                         />
-                        <h4 className="text">
+                        <p className="text">
                         A comprehensive e-commerce website that offers a diverse range of clothing items, jewelry and electronics. Built with React.js, Bootstrap and Redux. 
-                        </h4>
+                        </p>
                         <NavLink className="btns" to="https://areke-ayo-e-commerce-site.netlify.app/">
                         View Project Live
                         </NavLink>
@@ -75,9 +75,9 @@ const Project = () => {
                         src="/assets/images/tracker.png"
                         alt="yj"
                         />
-                        <h4 className="text">
+                        <p className="text">
                         A Task Tracker that empowers users to efficiently manage their to-do lists helping individuals stay organized and productive in their daily lives.
-                        </h4>
+                        </p>
                         <NavLink className="btns" to="https://areke-ayo-task-tracker.netlify.app/">
                         View Project Live
                         </NavLink>
